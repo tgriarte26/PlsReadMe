61dc18d490a89a923152490de8c149dabacee663
trevor <trevorgriarte@gmail.com>
2026-01-18 21:40:46 -0800

    implemented the header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="bg-[#24292e] justify-items-center">
+          <h1 className="font-mono text-4xl p-5 text-gray-300">PlsReadMe</h1>
+      </div>
+      <div className="p-0.5 bg-gray-300"></div>
+    </header>
+  );
+}
